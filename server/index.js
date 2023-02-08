847715dcc0ed2f38331bbe3983bcda71b23f140e
villalb4 <villalb4angel@gmail.com>
2023-02-08 18:31:03 -0300

    chagne: / response console.log

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 3001
 const server = app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}!`)
+    console.log(`Server listening on http://localhost:${PORT}/`)
 })
 
 module.exports = { server }
