bf50f12f8c70084c71842c26b4341993442b2e87
villalb4 <villalb4angel@gmail.com>
2023-02-08 18:30:42 -0300

    add: response json

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,6 @@ const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.get('/', (req, res) => res.send('Hello Worldl!'))
+app.get('/', (req, res) => res.json({api:'http://localhost:3001/api'}))
 
 module.exports = app
