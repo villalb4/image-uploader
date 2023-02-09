b82a3290d8f3a3145261471deaa91d510ee9e13d
villalb4 <villalb4angel@gmail.com>
2023-02-08 23:45:01 -0300

    refactor

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 3001
 const server = app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}/`)
+    console.log(`Server listening on port ${PORT}/`)
 })
 
-module.exports = { server }
+module.exports = server
