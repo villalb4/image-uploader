0fdce9c9c5646cdde10b118dfe4d834c80013e0c
Angel Villalba <villalb4angel@gmail.com>
2023-02-09 05:11:35 -0300

    add: import Upload component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import LoadingIndicator from './Components/LoadingIndicator/LoadingIndicator';
+// import LoadingIndicator from './Components/LoadingIndicator/LoadingIndicator';
+import Upload from './Pages/Upload/Upload';
 
 function App() {
   return (
     <div className="App">
-        <LoadingIndicator />
+        {/* <LoadingIndicator /> */}
+        <Upload />
     </div>
   );
 }
