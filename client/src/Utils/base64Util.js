b83dd9edc224eb68ae4bf7389aee9084fdb582ca
Angel Villalba <villalb4angel@gmail.com>
2023-04-22 16:32:24 -0300

    delete: files

--- client/src/Utils/base64Util.js
+++
@@ -1,9 +0,0 @@
-const base64Util = (image) => {
-    const reader = new FileReader()
-	reader.onload = (e) => {
-		const resultado = e
-	}
-	reader.readAsDataURL(image)
-}
-
-export default base64Util
