84fa3cab9783d5eb8e5d65d66c6a37caf86445db
Angel Villalba <villalb4angel@gmail.com>
2023-04-23 17:58:36 -0300

    fix: componente ordenado

--- client/src/Pages/Upload/Upload.tsx
+++ client/src/Pages/Upload/Upload.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import DropArea from "../../Components/DropArea/DropArea";
 import "./Upload.css";
-import LoadingIndicator from "../../Components/LoadingIndicator/LoadingIndicator";
 
 function Upload() {
   return (
-    <div className={true ? "Upload": "Upload loading"}>
-      {true ? (
-        <>
-          <h1>Upload your image</h1>
-          <p className="Upload__paragraph">File should be Jpeg, Png,...</p>
-          <DropArea />
-        </>
-      ) : (
-        <LoadingIndicator />
-      )}
+    <div>
+      <h1>Upload your image</h1>
+      <p className="Upload__paragraph">File should be Jpeg, Png,...</p>
+      <DropArea />
     </div>
   );
 }
