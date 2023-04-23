af97c5254ef2bd03be5023b4e989a57a49935668
Angel Villalba <villalb4angel@gmail.com>
2023-04-23 17:59:59 -0300

    feat: Preview component

---
+++ client/src/Components/Preview/Preview.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Preview.css"
+
+function Preview() {
+  return (
+    <div className='preview'>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Preview
