546cea7c87daac6d98f19557844e9fa09faf8d2d
Angel Villalba <villalb4angel@gmail.com>
2023-04-29 18:28:31 -0300

    modularization

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
+const uploadRouter = require("./routes/upload")
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(cors())
 
-app.get('/', (req, res) => res.json({api:'http://localhost:3001/api'}))
+app.use("/upload", uploadRouter)
 
 module.exports = app
