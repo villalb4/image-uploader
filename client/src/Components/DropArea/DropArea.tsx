35d0b9945b6b56c2d0eeded5e43170e18abd81cd
Angel Villalba <villalb4angel@gmail.com>
2023-04-29 18:30:15 -0300

    modularization y prettier format

--- client/src/Components/DropArea/DropArea.tsx
+++ client/src/Components/DropArea/DropArea.tsx
@@ -1,52 +1,48 @@
-import React, { useRef, useState } from 'react'
-import './DropArea.css'
-import mountains from '../../Assets/image.svg'
-import dashed from '../../Assets/dashed.svg'
-import UseImageConvert from '../../Hooks/useImageConvert'
-
+import React, { useRef, useState } from "react";
+import "./DropArea.css";
+import mountains from "../../Assets/image.svg";
+import dashed from "../../Assets/dashed.svg";
+import UseImageConvert from "../../Hooks/useImageConvert";
+import UseUpload from "../../Hooks/useUpload";
 
 type InputRef = React.RefObject<HTMLInputElement>;
 
 const DropArea = () => {
-	const inputRef: InputRef = useRef(null)
-	const [drag, setDrag] = useState(false)
-	const {imageBase64, handleImageChange} = UseImageConvert()
+  const inputRef: InputRef = useRef(null);
+  const [drag, setDrag] = useState(false);
+  const { imageBase64, handleImageChange } = UseImageConvert();
+	UseUpload(imageBase64.dataURL || "");
 
-	const handleOpenFile = () => {
-		inputRef?.current?.click()
-	}
+  const handleOpenFile = () => {
+    inputRef?.current?.click();
+  };
 
-	const handleUpload = () => {
-		console.log("linea 22: ", imageBase64)
-	}
-	imageBase64.size !== 0 && handleUpload()
-	
 
-	return (
-		<div className='DropArea'>
-			<div 
-			className={drag ? 'DropArea__dashed active' : 'DropArea__dashed'}
-			style={{backgroundImage: `url(${dashed})`}}
-			>
-				<div>
-					<img src={mountains} alt="" />
-				</div>
-				<span>Drag & Drop your image here</span>
-				<input 
-					type="file" 
-					accept="image/png, image/jpeg, image/jpg, image/gif"
-					ref={inputRef} 
-					onChange={handleImageChange}
-					onDragEnter={() => setDrag(true)}
-					onDragLeave={() => setDrag(false)}
-				/>
-			</div>
-			<span className='DropArea__or'>Or</span>
-			<button className='DropArea__ChooseFile' onClick={handleOpenFile}>
-				Choose a file
-			</button>
-		</div>
-  )
-}
+  return (
+    <div className="DropArea">
+      <div
+        className={drag ? "DropArea__dashed active" : "DropArea__dashed"}
+        style={{ backgroundImage: `url(${dashed})` }}
+      >
+        <div>
+          <img src={mountains} alt="" />
+        </div>
+        <span>Drag & Drop your image here</span>
+        <input
+          type="file"
+          accept="image/png, image/jpeg, image/jpg, image/gif"
+          ref={inputRef}
+          onChange={handleImageChange}
+          onDragEnter={() => setDrag(true)}
+          onDragLeave={() => setDrag(false)}
+        />
+      </div>
+      <span className="DropArea__or">Or</span>
+      <button className="DropArea__ChooseFile" onClick={handleOpenFile}>
+        Choose a file
+      </button>
+    </div>
+  );
+};
 
-export default DropArea
+export default DropArea;
