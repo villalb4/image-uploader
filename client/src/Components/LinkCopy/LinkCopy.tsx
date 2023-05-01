fbe029aa7b4841cd2538516f9559b66f387e8aa1
Angel Villalba <villalb4angel@gmail.com>
2023-05-01 18:07:52 -0300

    feat: reemplazando hardcodeo por info real

--- client/src/Components/LinkCopy/LinkCopy.tsx
+++ client/src/Components/LinkCopy/LinkCopy.tsx
@@ -1,15 +1,13 @@
-import React from "react";
+import { RootState } from "../../Redux/store/store";
 import "./LinkCopy.css";
+import { useSelector } from "react-redux/es/exports";
 
 function LinkCopy() {
+  const imageUrl = useSelector((state: RootState) => state.image.imageUrl);
   return (
     <div className="LinkCopy">
-        <span className="LinkCopy__link">
-          https://images.yourdomain.com/photo-1496950866446-325...
-        </span>
-
-        <button className="LinkCopy__button">Copy Link</button>
-      
+      <span className="LinkCopy__link">{imageUrl}</span>
+      <button className="LinkCopy__button">Copy Link</button>
     </div>
   );
 }
--- client/src/Components/Preview/Preview.tsx
+++ client/src/Components/Preview/Preview.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { RootState } from "../../Redux/store/store";
 import "./Preview.css"
+import { useSelector } from "react-redux/es/exports";
 
 function Preview() {
+
+  const image = useSelector((state: RootState) => state.image.imageUrl)
+
   return (
     <div className='preview'>
       <div>
-        <img src="" alt="" />
+        <img src={image} alt="imagen cargada" />
       </div>
     </div>
   )
