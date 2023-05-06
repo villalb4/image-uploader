63df381c17b0be1c19a3f858b58ca3dad762baa8
Angel Villalba <villalb4angel@gmail.com>
2023-05-06 16:18:06 -0300

    fix: format file

--- client/src/Redux/thunks/submitImage.ts
+++ client/src/Redux/thunks/submitImage.ts
@@ -1,26 +1,26 @@
 import { setImage, setStage } from "../slices/image";
 import { AppDispatch, RootState } from "../store/store";
 
-export const submitImage = (image : string) => {
+export const submitImage = (image: string) => {
   return async (dispatch: AppDispatch, state: RootState) => {
+    try {
+      dispatch(setStage("submit"));
 
-    dispatch(setStage("submit"))
+      const response = await fetch("http://localhost:3010/upload", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ image: image }),
+      });
 
-    const response = await fetch("http://localhost:3010/upload", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ image: image }),
-    });
+      dispatch(setStage("loaded"));
+      const responseBody = await response.json();
 
-    if(response.ok) {
-      dispatch(setStage("loaded"))
+      dispatch(setImage(responseBody.url));
+    } catch (error) {
+      dispatch(setStage("error"))
+      console.log(error);
     }
-
-    const responseBody = await response.json()
-
-    dispatch(setImage(responseBody.url))
-
-  }
-}
+  };
+};
