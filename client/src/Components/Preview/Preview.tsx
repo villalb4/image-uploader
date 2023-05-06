00e1bf72f5b2cd274af7e14fd02606c7ccc0eba8
Angel Villalba <villalb4angel@gmail.com>
2023-05-06 16:14:09 -0300

    fix: image

--- client/src/Components/Preview/Preview.tsx
+++ client/src/Components/Preview/Preview.tsx
@@ -1,18 +1,18 @@
 import { RootState } from "../../Redux/store/store";
-import "./Preview.css"
+import "./Preview.css";
 import { useSelector } from "react-redux/es/exports";
 
 function Preview() {
-
-  const image = useSelector((state: RootState) => state.image.imageUrl)
+  const image = useSelector((state: RootState) => state.image.imageUrl);
 
   return (
-    <div className='preview'>
-      <div className='preview_divContainer'>
-        <img className='preview_image' src={image} alt="imagen cargada" />
-      </div>
+    <div className="preview">
+      <div
+        className="preview_divContainer"
+        style={{ backgroundImage: `url(${image})`}}
+      ></div>
     </div>
-  )
+  );
 }
 
-export default Preview
+export default Preview;
